472c4c0eaffa51ef3329e42340175188cc0fd4e4
YoungMinds <ygminds73@gmail.com>
2024-11-17 19:28:20 +0530

    first commit

---
+++ Script4.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#This is creation and calling of shell variable --- defining variables
+Class=Young-minds
+Batch=25
+PROFESSION=AWS/DevSecOps/Azure DevOps
+echo "Class Name is $Class, Batch number $Batch, We are learning $PROFESSION"
