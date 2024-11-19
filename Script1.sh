d5fa103902cbd10825070cb8d637159735f20574
Young Minds <109673364+ygminds73@users.noreply.github.com>
2024-11-19 08:02:42 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -1,4 +1,4 @@
 echo "We are trying this practical from linux EC2"
-echo "We are from batch-25 and learning git/gihub"
+echo "I am from Young minds"
 echo "We are from batch-26 and learning DevOps"
 echo "Hello, Good morning!"
