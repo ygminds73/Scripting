0c1cab99f9edd18880e0426ecb99df0ef549e958
Avdhut <avdhutredekar12@gmail.com>
2024-12-25 17:06:37 +0530

    NEW

--- Script1.sh
+++ Script1.sh
@@ -2,11 +2,5 @@ echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
 echo "We are from batch-26 and learning DevOps"
 echo "Hello, Good morning!"
-"this is new testing of pull request over git"
-"Hope it will work"
-for i in range(10):
-    print(I know python)
-
-"Thats set :) "
 
  
