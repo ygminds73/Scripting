365b67a9b4c6fe1efa1549135df66a07f2649965
Avdhut <avdhutredekar12@gmail.com>
2024-12-25 16:24:37 +0530

    Saving new script1 file as modified

--- Script1.sh
+++ Script1.sh
@@ -2,3 +2,6 @@ echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
 echo "We are from batch-26 and learning DevOps"
 echo "Hello, Good morning!"
+"this is new testing of pull request over git"
+"Hope it will work"
+
