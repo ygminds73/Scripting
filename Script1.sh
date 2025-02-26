72a720e53c98b4b4742791ff97a880c7f239433f
Pradhyumn1998 <vairagep@gmail.com>
2025-02-26 22:57:36 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -1,4 +1,4 @@
 echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
 echo "We are from batch-27 and learning DevOps"
-echo "marge data REQ1234 to master"
+echo "This updates from Branch-B"
