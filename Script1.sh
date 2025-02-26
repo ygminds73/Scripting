607ccf4493e3156d4653f5d13b50f85f7614bb59
AtulWankhede1 <wankhedeatul1989@gmail.com>
2025-02-27 00:03:22 +0530

    this is my commit to fix the issue

--- Script1.sh
+++ Script1.sh
@@ -1,5 +1,3 @@
 echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
 echo "We are from batch-27 and learning DevOps"
-echo "I am working in REQ0948"
-echo " i am atul wankhede"
