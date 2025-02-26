bb6fdf8986f4a997e8db35c4303967fdc89f78c3
Pradhyumn1998 <vairagep@gmail.com>
2025-02-26 22:28:33 +0530

    REQ1234

--- Script1.sh
+++ Script1.sh
@@ -1,4 +1,4 @@
 echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
 echo "We are from batch-27 and learning DevOps"
-echo "I am working on my first VS code project"
+echo "marge data from REQ1234 to master"
