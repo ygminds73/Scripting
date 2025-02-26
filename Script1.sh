e671643b1982a3b1d836e97ebdcd16ab843e2b2e
Pranavi Shelke <pranavishelke07@gmail.com>
2025-02-26 15:31:11 +0530

    First commit by Pranavi

--- Script1.sh
+++ Script1.sh
@@ -1,7 +1,4 @@
 echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
 echo "We are from batch-27 and learning DevOps"
-echo "I am working in REQ092348"
-echo "Adding new data"
-echo "I am DevOps-2"
-echo "Adding new line"
+echo "Changed by Pranavi"
