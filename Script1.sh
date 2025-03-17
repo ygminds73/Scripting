02f3e130457fe46f64a4827b9c68b2523db19940
Pranavi Shelke <pranavishelke07@gmail.com>
2025-03-17 09:18:55 +0530

    This is for practice

--- Script1.sh
+++ Script1.sh
@@ -1,6 +1,3 @@
 echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
 echo "We are from batch-27 and learning DevOps"
-echo "Changed by Pranavi"
-echo " this is updated by shraddha"
-echo "Practice Branching"
