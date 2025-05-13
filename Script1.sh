81280d77597772a89f370cd4f200258e8faa5396
YoungMinds <ygminds73@gmail.com>
2025-05-13 07:59:15 +0530

    this is my first commit for batch-28

--- Script1.sh
+++ Script1.sh
@@ -1,5 +1,4 @@
 echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
 echo "We are from batch-27 and learning DevOps"
-echo "I am working in REQ092348"
-echo "I have learnt Github and Git"
+echo "We are learning Git and GitHub"
