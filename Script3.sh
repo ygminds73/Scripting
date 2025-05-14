496f400d2fc3a7020f7e56cf013da2f852bca326
Atul_Rajdev <atulrajdev003@gmail.com>
2025-05-14 04:49:22 -0700

    my git hub acc

--- Script3.sh
+++ Script3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #this is to check if the shell scripts picks up env variables
 echo "This is error script"   # sample script
-echo "This is my system path $PATH"
+echo "This is mysystem path $PATH"
 
 export JDK_HOME=/bin/jde
 echo "my new JDK home is=$JDK_HOME"
