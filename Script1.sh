fe92cf9a566e3382bf896f9c045c7aee930cce51
Bhagyashree Sonawane <bhagyashrigurav95@gmail.com>
2025-05-14 14:53:42 +0530

    new commit

--- Script1.sh
+++ Script1.sh
@@ -1,6 +1,6 @@
 echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
-echo "We are from batch-27 and learning DevOps"
+echo "We are from batch-28 and learning DevOps"
 echo "We are learning Git and GitHub"
-echo "How are you?"
 echo "I am learning branching in Git"
+echo "Here we use the git commands"
