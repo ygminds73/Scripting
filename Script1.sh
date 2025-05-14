9f212e1b33ecb0ad272a1cdc4c465b23314597c0
Abheek246 <abheeksrivastava36@gmail.com>
2025-05-15 01:01:29 +0530

    Branching

--- Script1.sh
+++ Script1.sh
@@ -4,6 +4,6 @@ echo "We are from batch-27 and learning DevOps"
 echo "We are learning Git and GitHub"
 echo "How are you?"
 echo "I am good"
-echo "Hoe are you "
-echo " bye abheek"
-echo "Hi abheek
+echo "How are you "
+echo " I am learning branching"
+
