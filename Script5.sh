706ed89d9582891ae5d9e27e19cf246fb80c88f8
skggote <skggote@gmail.com>
2025-05-17 22:25:40 +0530

    unnati

--- Script5.sh
+++ Script5.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #This script is to make variable read only, means we cannot set the value of NAME variable again
-NAME=Young-Minds
+NAME=Unnati
 readonly NAME
 NAME=DEVOPS
 echo "my name is: $NAME"
