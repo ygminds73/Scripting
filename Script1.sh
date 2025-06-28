14cdb8f9a2cb48561ba217256c4855a107fd447c
Vipul228 <vipulnandnikar4@gmail.com>
2025-06-28 18:46:37 +0530

    Changes made on 28 June

--- Script1.sh
+++ Script1.sh
@@ -5,3 +5,5 @@ echo  " I am  learning  branching"
 echo "Creation of the new branch"
 echo "Changing in the new branch"
 echo "new Commit"
+echo  Doing the changes for the new commit in the git for git hub"
+
