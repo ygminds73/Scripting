3afede5c0734932dcfd2cfdf6b271acff3b662b5
UnnatiGote <119988138+UnnatiGote@users.noreply.github.com>
2025-07-15 16:15:41 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -2,6 +2,6 @@ echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
 echo "We are from batch-27 and learning DevOps"
 echo "We are learning Git and GitHub"
-
+echo " hi batch- 28"
 
 
