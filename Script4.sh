71e579ab12d307eabe92db17e88351cc9d366494
Sneha <shaharesp@gmail.com>
2025-08-06 16:25:18 +0530

    This is First Commit

--- Script4.sh
+++ Script4.sh
@@ -4,4 +4,4 @@ Class=Young-minds
 Batch=25
 PROFESSION=AWS/DevSecOps/Azure DevOps
 echo "Class Name is $Class, Batch number $Batch, We are learning $PROFESSION"
-echo "batch-29"
+echo "batch-29 devops" 
