8002f0fca570f375e78b2471a388841c0b578445
yg-batch-29 <prachibagul98@gmail.com>
2025-08-07 20:10:25 +0530

    latest changes

--- Script4.sh
+++ Script4.sh
@@ -2,5 +2,4 @@
 #This is creation and calling of shell variable --- defining variables
 Class=Young-minds
 Batch=25
-PROFESSION=AWS/DevSecOps/Azure DevOps
-echo "Class Name is $Class, Batch number $Batch, We are learning $PROFESSION"
+
