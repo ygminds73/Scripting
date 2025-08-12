d0ed34917776409ded985839d8851feb023b1207
Rohan-9009 <159605391+Rohan-9009@users.noreply.github.com>
2025-08-12 20:16:10 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -5,3 +5,6 @@ echo "I am learning Git/Github"
 echo "I am learning DevOps"
 echo "I am working on branch-29"
 
+echo "We are trying this practical from linux EC2"
+echo "I am from Young minds"
+echo "We are from batch-27" 
