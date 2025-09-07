9039df091848346d0667f4f8bc262779ab1cea26
AKASH-8329 <akashthorat097@gmail.com>
2025-09-07 15:51:10 +0530

    commiting changes

--- Script1.sh
+++ Script1.sh
@@ -2,6 +2,4 @@ echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
 echo "We are from batch-27" 
 echo "I am learning Git/Github"
-echo "I am learning DevOps"
-echo "I am working on branch-1"
-echo "hello i am akash "
+
