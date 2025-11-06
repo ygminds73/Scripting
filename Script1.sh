a0937a90411266af4ed5fd5892a0eac5f5a3543a
AsmitaKurandale <asmitakurandale2000@gmail..com>
2025-11-06 18:12:43 +0530

    i have made changes in script1 file

--- Script1.sh
+++ Script1.sh
@@ -1,5 +1,4 @@
 echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
-echo "We are from batch-27" 
-echo "I am learning Git/Github"
-echo "I have added new code"
+echo "We are from batch-27"
+echo "Hii i am learning git niw"
