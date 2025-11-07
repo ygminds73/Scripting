6e0113b71c67ad7f8f04cb5bd56b268a596ada2e
Young Minds <109673364+ygminds73@users.noreply.github.com>
2025-11-07 07:43:52 +0530

    Remove line numbers from echo commands

--- Script1.sh
+++ Script1.sh
@@ -1,4 +1,4 @@
-1.echo "We are trying this practical from linux EC2"
-2.echo "We are from batch-25 and learning git/gihub"
-3.echo "We are learning Branching"
-4.echo "I am learning Devops"
+echo "We are trying this practical from linux EC2"
+echo "We are from batch-25 and learning git/gihub"
+echo "We are learning Branching"
+echo "I am learning Devops"
