ec7bc9001aca610fcf24bb3d7d96392db0a5f981
rup197 <61383301+rup197@users.noreply.github.com>
2025-11-07 21:22:54 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -2,7 +2,3 @@ echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "We are learning Branching"
 echo "I am learning Devops"
-echo "Adding new line"
-echo "I am from Devops and cloud team"
-echo "I am DevOps-2"
-
