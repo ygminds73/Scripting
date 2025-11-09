32dc36bc11fe6e1ae75e7bf3085c30815828ab65
sanketamte123 <sanketamtesank@gmail.com>
2025-11-09 22:14:35 +0530

    first commit

--- Script1.sh
+++ Script1.sh
@@ -2,6 +2,6 @@ echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
 echo "We are from batch-27" 
 echo "I am learning Git/Github"
-echo "I have added "
-echo "gyifuj bjghionkSS"S
+echo "I have added"
+ech0 "hii"
 
