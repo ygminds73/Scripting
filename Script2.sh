fcc984023ff931f5209947d93cb2a1b6be028eba
rhtaiwale <rhtusertecho@gmail.com>
2025-11-10 15:24:48 +0530

    code changed

--- Script2.sh
+++ Script2.sh
@@ -8,4 +8,5 @@ echo "Hello value of a is $a and value of b is $b"
 echo "hello the file have some changes"
 echo " the point 8 is correct"
 echo " the point 9 also correct"
+echo " the change happened"
 
