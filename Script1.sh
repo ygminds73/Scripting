6ab5db278b9e25f824d17733ef316720867d5c0a
Ishwari <ishwaritajane@gmail.com>
2025-11-17 15:41:20 +0530

    i have done some changes

--- Script1.sh
+++ Script1.sh
@@ -2,7 +2,7 @@ echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "We are learning Branching"
 echo "I am learning Devops"
-echo "Adding new line"
+echo "creating branch"
 echo "I am learning branching"
 
 
