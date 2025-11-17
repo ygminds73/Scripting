d8d083a2f9057f9474c903af22961d6e55b1ef69
Ishwari <ishwaritajane@gmail.com>
2025-11-17 15:25:41 +0530

    i have done changes for incident INC0902324

--- Script1.sh
+++ Script1.sh
@@ -3,5 +3,6 @@ echo "We are from batch-25 and learning git/gihub"
 echo "We are learning Branching"
 echo "I am learning Devops"
 echo "Adding new line"
-echo "I am from Devops and cloud team"
+echo "I am learning branching"
+
 
