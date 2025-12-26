9f023102bb59079f75b6267dd75c274fbe77c2b5
Young Minds <109673364+ygminds73@users.noreply.github.com>
2025-12-26 09:14:42 +0530

    Update Script19.sh

--- Script19.sh
+++ Script19.sh
@@ -1,1 +1,10 @@
-echo "I have created a new file"
+#!/bin/sh
+
+file="test.txt"
+
+if [ -f "$file" ]
+then
+  echo "File '$file' exists and is a regular file."
+else
+  echo "File '$file' does not exist."
+fi
