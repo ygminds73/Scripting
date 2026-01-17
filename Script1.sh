018ddf53a9a7fd67b57b323b75b4db61c1bd87df
Akshata's Mac <abhi@Abhis-MacBook-Air.local>
2026-01-17 23:22:47 +0530

    This is my first commit in batch-29

--- Script1.sh
+++ Script1.sh
@@ -1,5 +1,5 @@
 echo "We are trying this practical from linux EC2"
 echo "I am from Young minds"
 echo "We are from batch-27 and learning DevOps"
-echo "We are learning AWS, Azure DevOps and DevSecOps"
 echo "I am learning in batch-29"
+echo "Hello my first modification"
