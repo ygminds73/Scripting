e49ef2bfded576df20d3e83b6d9546d9f43fe6af
Akshata's Mac <abhi@Abhis-MacBook-Air.local>
2026-01-18 20:00:23 +0530

    committed the changes

--- Script4.sh
+++ Script4.sh
@@ -4,4 +4,3 @@ Class=Young-minds
 Batch=25
 PROFESSION=AWS/DevSecOps/Azure DevOps
 echo "Class Name is $Class, Batch number $Batch, We are learning $PROFESSION"
-echo "We are trying this practical from linux EC2"git
