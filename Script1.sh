a973c5d1749f04eedaed990924f23221e7bf846c
naktimithun94 <naktimithun94@gmail.com>
2026-02-18 09:20:37 +0530

    this is my 2nd commit

--- Script1.sh
+++ Script1.sh
@@ -5,4 +5,5 @@ echo "I am learning Devops"
 echo "Adding new line"
 echo "I am from Devops and cloud team"
 echo "I am learning Devops"
-echo "I AM New here"
+echo "I AM New here"git
+echo "this is my 2nd commit"
