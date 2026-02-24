a79c7953390bf92d044e5dc9483a4c749c09c6ad
bansoderutuja <bansoderutuja1234@gmail.com>
2026-02-24 16:51:35 +0530

    1st commit of 24

--- Script1.sh
+++ Script1.sh
@@ -2,6 +2,6 @@ echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "We are learning Branching"
 echo "I am learning Git"
-echo "Hello, How are you?"
-echo "This is new text added to script"
+echo "This is 24 feb 2025"
+
 
