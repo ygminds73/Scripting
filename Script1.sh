6627a0b446a33f2b04fe84304c5b06ad49d10454
bansoderutuja <bansoderutuja1234@gmail.com>
2026-02-24 17:01:24 +0530

    Conflict check

--- Script1.sh
+++ Script1.sh
@@ -2,6 +2,5 @@ echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
 echo "We are learning Branching"
 echo "I am learning Git"
-echo "Hello, How are you?"
-echo "This is new text added to script"
+echo "checking Conflict"
 
