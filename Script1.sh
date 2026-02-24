49d71aebd8159fe1cd46443cf101b1e26e7972c0
ksiddhesh99@gmail.com <ksiddhesh99@gmail.com>
2026-02-24 23:58:20 +0530

    My First Commit

--- Script1.sh
+++ Script1.sh
@@ -1,4 +1,3 @@
-echo "We are trying this practical from linux EC2"
-echo "We are from batch-25 and learning git/gihub"
-echo "We are learning Branching"
 echo "I am learning Git"
+echo "My Name is Siddhesh"
+echo "I am learning Devops"
