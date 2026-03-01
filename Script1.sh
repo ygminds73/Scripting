d85a9bc2790e76eb401ac2ce0bd7c1210ad93cec
bansoderutuja <bansoderutuja1234@gmail.com>
2026-03-01 17:32:22 +0530

    1

--- Script1.sh
+++ Script1.sh
@@ -1,7 +1,5 @@
 echo "We are trying this practical from linux EC2"
 echo "We are from batch-25 and learning git/gihub"
-echo "We are learning Branching"vvvvvvbvvvvv
-echo "I am learning Git"
-echo "Hello, How are you?"
-echo "Im trying my best to do the work"
+echo "We are learning Branching"
+
 
